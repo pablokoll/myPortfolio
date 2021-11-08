f8fc13dbd4437b65e57ec66bd86db7d0f7990973
PabloK09 <pablokollm@gmail.com>
2021-11-08 17:57:13 -0300

    90%

--- src/assets/helpers/projects/bffGallery.js
+++ src/assets/helpers/projects/bffGallery.js
@@ -21,34 +21,23 @@ function BffGallery() {
         },
     ];
 
-    const settings = {
-        centerMode: false,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 8000,
-        arrows: false,
-        swipeToSlide: true,
-    };
-
     return (
         <div className='flex relative items-center min-h-screen w-full '>
-            <span className='absolute top-6 md:top-16 md:left-1/4 left-3'>
-                <Link to='/'>
-                    <BsFillArrowLeftCircleFill className='h-8 w-8 text-myDarkGray hover:text-myBlue' />
-                </Link>
-            </span>
-            <div className='flex flex-col md:container px-6 py-5 md:mx-auto items-center md:w-2/3 w-full h-full'>
-                <div className='flex items-center gap-5'>
-                    <h2 className='title-font lg:text-5xl sm:text-4xl text-3xl mb-4 font-extrabold text-myDarkGray relative'>
-                        BFF Gallery
-                    </h2>
-                    <span className='text-myBlue font-bold text-shadow'>Dog Gallery</span>
+            <div className='flex flex-col md:container px-5 lg:px-16 xl:px-6 py-5 md:mx-auto items-center xl:w-3/4 w-full h-full '>
+                <div className='flex items-center gap-5 relative w-full xl:w-3/4 justify-center'>
+                    <span className='self-start absolute left-0 xl-left-20'>
+                        <Link to='/'>
+                            <BsFillArrowLeftCircleFill className='h-8 w-8 text-myDarkGray hover:text-myBlue' />
+                        </Link>
+                    </span>
+                    <div className='flex items-center flex-col sm:flex-row mb-4 justify-center'>
+                        <h2 className='title-font lg:text-5xl sm:text-4xl text-3xl font-extrabold text-myDarkGray relative whitespace-nowrap'>
+                            BFF Gallery
+                        </h2>
+                        <span className='text-myBlue font-bold text-shadow items-center justify-center ml-2'>Dog Gallery</span>
+                    </div>
                 </div>
-                <div className='flex relative md:w-2/3 w-full justify-center'>
+                <div className='flex relative xl:w-3/4 w-full justify-center'>
                     <iframe
                         className='rounded-2xl w-full'
                         width='500'
@@ -60,68 +49,56 @@ function BffGallery() {
                         allowfullscreen
                     ></iframe>
                 </div>
-                <div className='flex flex-col md:w-2/3 justify-center items-center py-2'>
-                    <div>
-                        <div className='flex flex-col mt-4'>
-                            <h5 className='text-myBlue font-semibold text-xl text-shadow'>
-                                JAVASCRIPT
-                            </h5>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-2'>
-                                Technologies
-                            </h4>
-                            <div className='flex gap-5 items-center w-full'>
-                                <Slider
-                                    {...settings}
-                                    className='w-full items-center justify-center relative mb-10'
-                                >
-                                    {skillArr.map((education, index) => {
-                                        return (
-                                            <div className='flex flex-col items-center justify-center'>
-                                                <h2 className='text-mySkillSubSkill font-bold xl:text-2xl sm:text-xl text-md text-left'>
-                                                    {education.title}
-                                                </h2>
-                                                <div
-                                                    key={index}
-                                                    className='mt-2 flex flex-col bg-white p-3 rounded-xl h-36 relative shadow-md text-left '
-                                                >
-                                                    <p className='text-myDarkGray font-semibold sm:text-xl text-md'>
-                                                        {education.skills.map((skill) => {
-                                                            return <p>{skill}</p>;
-                                                        })}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        );
-                                    })}
-                                </Slider>
+                <div className='flex flex-col xl:w-3/4 w-full justify-center items-center py-2'>
+                    <div className='flex flex-col mt-4 w-full'>
+                        <h5 className='text-myBlue font-semibold text-xl text-shadow'>
+                            JAVASCRIPT
+                        </h5>
+                        <h4 className='text-myDarkGray font-bold text-3xl mb-2'>Technologies</h4>
+                        <div className='flex items-center w-full'>
+                            <div className='w-full items-center justify-center relative mb-2 px-2'>
+                                {skillArr.map((education, index) => {
+                                    return (
+                                        <div className='flex flex-col 2xl:flex-row items-center mb-1 '>
+                                            <h2 className='text-myNightBlue font-bold xl:text-2xl sm:text-xl text-md text-left mr-2 self-start whitespace-nowrap'>
+                                                {education.title}
+                                            </h2>
+                                            <p className='flex flex-wrap 2xl:flex-nowrap self-start'>
+                                                {education.skills.map((skill) => {
+                                                    return (
+                                                        <span className='text-myDarkGra mr-2 font-semibold sm:text-lg text-md 2xl:whitespace-nowrap'>
+                                                            {skill}
+                                                        </span>
+                                                    );
+                                                })}
+                                            </p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
-                        </div>
-                        <div className='flex flex-col mt-4'>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-4'>Description</h4>
-                            <p className='text-left px-2'>
-                                BFF-Gallery is a dog-friendly of different breeds, in there you can
-                                se than more 172 breed. In the APP you can filter for the
-                                temperaments of breeds, searching a breed with name, order
-                                alphabetical ascendant or descendant, order for the range weight of
-                                dogs and filter breeds of the API or the database. Also you can
-                                create your fantasy breed this is a controlled form, with
-                                validation. When you create a breed you can choose a image, name,
-                                weight and others characteristics. The APP counts with a section of
-                                favorites, and you can visualize on detail all breeds!
-                            </p>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-4 mt-4'>
-                                Experience
-                            </h4>
-                            <p className='text-left px-2'>
-                                This is my first project full Stack, I codivd learning a lot of
-                                things for example the magic connection between the Front end and
-                                Back end, using a API with my database and sending both datas. I
-                                have a lot of appreciation for this project because it made me
-                                understand many things, unite all the things I had learned and
-                                obtain this resdivt being the first time, I am really very happy
-                                with what was obtained.
-                            </p>
-                        </div>
+                    </div>
+                    <div className='flex flex-col mt-4'>
+                        <h4 className='text-myDarkGray font-bold text-3xl mb-4'>Description</h4>
+                        <p className='text-left px-2'>
+                            BFF-Gallery is a dog-friendly of different breeds, in there you can se
+                            than more 172 breed. In the APP you can filter for the temperaments of
+                            breeds, searching a breed with name, order alphabetical ascendant or
+                            descendant, order for the range weight of dogs and filter breeds of the
+                            API or the database. Also you can create your fantasy breed this is a
+                            controlled form, with validation. When you create a breed you can choose
+                            a image, name, weight and others characteristics. The APP counts with a
+                            section of favorites, and you can visualize on detail all breeds!
+                        </p>
+                        <h4 className='text-myDarkGray font-bold text-3xl mb-4 mt-4'>Experience</h4>
+                        <p className='text-left px-2'>
+                            This is my first project full Stack, I codivd learning a lot of things
+                            for example the magic connection between the Front end and Back end,
+                            using a API with my database and sending both datas. I have a lot of
+                            appreciation for this project because it made me understand many things,
+                            unite all the things I had learned and obtain this resdivt being the
+                            first time, I am really very happy with what was obtained.
+                        </p>
                     </div>
                 </div>
             </div>
--- src/assets/helpers/projects/cocoMall.js
+++ src/assets/helpers/projects/cocoMall.js
@@ -7,107 +7,109 @@ import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
 // import cocoMallCart from '../../img/coco-mall/coco-mall cart.png'
 
 function CocoMall() {
+    const skillArr = [
+        {
+            title: 'FRONT END',
+            skills: ['HTML', 'CSS', 'React (Hooks)', 'Redux', 'Tailwind', 'Figma'],
+        },
+        {
+            title: 'BACK END',
+            skills: ['Node.js', 'Express', 'Sequelize', 'PostgreSQL'],
+        },
+        {
+            title: 'TOOLS',
+            skills: ['Git', 'GitHub', 'Firebase', 'Mercado Pago', 'Google Maps API', 'Heroku', 'Vercel'],
+        },
+    ];
+
     return (
-        <div className='flex relative items-center min-h-screen'>
-            <span className='absolute top-6 md:top-16 md:left-1/4 left-2'>
+
+<div className='flex relative items-center min-h-screen w-full '>
+    <div className='flex flex-col md:container px-5 lg:px-16 xl:px-6 py-5 md:mx-auto items-center xl:w-3/4 w-full h-full '>
+        <div className='flex items-center gap-5 relative w-full xl:w-3/4 justify-center'>
+            <span className='self-start absolute left-0 xl-left-20'>
                 <Link to='/'>
                     <BsFillArrowLeftCircleFill className='h-8 w-8 text-myDarkGray hover:text-myBlue' />
                 </Link>
             </span>
-            <div className='flex flex-col container px-5 py-5 mx-auto items-center  md:w-2/3 h-full'>
-                <div className='flex items-center gap-5 relative '>
-                    <h2 className='title-font lg:text-5xl sm:text-4xl text-3xl mb-4 pl-3 font-extrabold text-myDarkGray relative'>
-                        COCO mall
-                    </h2>
-                    <span className='text-myBlue font-bold text-shadow'>Marketplace</span>
-                </div>
-                <div className='flex relative md:w-2/3 justify-center'>
-                    <iframe
-                        className='rounded-2xl w-full'
-                        width='500'
-                        height='281'
-                        src='https://www.youtube.com/embed/Yr-K0FOLEFY'
-                        title='YouTube video player'
-                        frameborder='0'
-                        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                        allowfullscreen
-                    ></iframe>
-                </div>
-                <div className='flex flex-col md:w-2/3 justify-center items-center py-2'>
-                    <div>
-                        <div className='flex flex-col mt-4'>
-                            <h5 className='text-myBlue font-semibold text-xl text-shadow'>
-                                JAVASCRIPT
-                            </h5>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-2 '>
-                                Technologies
-                            </h4>
-                            <div className='flex justify-between items-center px-2'>
-                                <div>
-                                    <h4 className='text-myBlue font-bold text-xl bg-myButtonFill'>
-                                        Front-end
-                                    </h4>
-                                    <ul className='flex flex-col text-myDarkGray bg-white p-4 rounded-lg'>
-                                        <li className=''>React (Hooks)</li>
-                                        <li className=''>Redux</li>
-                                        <li className=''>Tailwind</li>
-                                        <li className=''>Figma</li>
-                                    </ul>
+            <div className='flex items-center flex-col sm:flex-row mb-4 justify-center'>
+                <h2 className='title-font lg:text-5xl sm:text-4xl text-3xl font-extrabold text-myDarkGray relative whitespace-nowrap'>
+                    COCO mall
+                </h2>
+                <span className='text-myBlue font-bold text-shadow items-center justify-center ml-2'>
+                    Marketplace
+                </span>
+            </div>
+        </div>
+        <div className='flex relative xl:w-3/4 w-full justify-center'>
+            <iframe
+                className='rounded-2xl w-full'
+                width='500'
+                height='281'
+                src='https://www.youtube.com/embed/Yr-K0FOLEFY'
+                title='YouTube video player'
+                frameborder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowfullscreen
+            ></iframe>
+        </div>
+        <div className='flex flex-col xl:w-3/4 w-full justify-center items-center py-2'>
+            <div className='flex flex-col mt-4 w-full'>
+                <h5 className='text-myBlue font-semibold text-xl text-shadow'>JAVASCRIPT</h5>
+                <h4 className='text-myDarkGray font-bold text-3xl mb-2'>Technologies</h4>
+                <div className='flex items-center w-full'>
+                    <div className='w-full items-center justify-center relative mb-2 px-2'>
+                        {skillArr.map((education, index) => {
+                            return (
+                                <div className='flex flex-col 2xl:flex-row items-center mb-1 '>
+                                    <h2 className='text-myNightBlue font-bold xl:text-2xl sm:text-xl text-md text-left mr-2 self-start whitespace-nowrap'>
+                                        {education.title}
+                                    </h2>
+                                    <p className='flex flex-wrap 2xl:flex-nowrap self-start'>
+                                        {education.skills.map((skill) => {
+                                            return (
+                                                <span className='text-myDarkGra mr-2 font-semibold sm:text-lg text-md 2xl:whitespace-nowrap'>
+                                                    {skill}
+                                                </span>
+                                            );
+                                        })}
+                                    </p>
                                 </div>
-                                <ul className='flex flex-col justify-start text-myDarkGray'>
-                                    <li className='text-myNightBlue font-semibold text-lg'>
-                                        Back-end
-                                    </li>
-                                    <li className=''>Node.js</li>
-                                    <li className=''>Express</li>
-                                    <li className=''>Sequelize</li>
-                                    <li className=''>PostgreSQL</li>
-                                </ul>
-                                <ul className='flex flex-col justify-start text-myDarkGray'>
-                                    <li className='text-myNightBlue font-semibold text-lg'>
-                                        Tools
-                                    </li>
-                                    <li className=''>GitHub</li>
-                                    <li className=''>Firebase</li>
-                                    <li className=''>Mercado Pago</li>
-                                    <li className=''>Google Maps API</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className='flex flex-col mt-4'>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-4'>Description</h4>
-                            <p className='text-left px-2 mb-1'>
-                                COCO mall is a marketplace for all your needs, we offer a
-                                possibility to enterprises can create yours shops in we platform, in
-                                this app you can post a lot of products, share the ubication of
-                                store with Google Maps API and management of stock. The user can
-                                explore the all stores with complete flexibility, can add to cart a
-                                products of different stores, and realize the order in one payment.
-                            </p>
-                            <p className='text-left px-2 mb-1'>
-                                Users can post reviews of the stores, and public questions in the
-                                products. The page count with panel of user and stores, in this
-                                place you can administrate a store, products, orders, user info,
-                                etc.. Also we have a user administrator he can control all the
-                                stores, products and users.
-                            </p>
-                            <h4 className='text-myDarkGray font-bold text-3xl mb-4 mt-4'>
-                                Experience
-                            </h4>
-                            <p className='text-left px-2 mb-1'>
-                                During the project, I learned different things, mainly teamwork.
-                                This work was done by 8 people, where we had 1 month to carry it
-                                out. Part of the learning was using the SCRUM methodology, version
-                                management with GitHub and the discussions of how we were going to
-                                organize the tasks. It is a project where I learned a lot and had
-                                fun doing it.
-                            </p>
-                        </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
+            <div className='flex flex-col mt-4'>
+                <h4 className='text-myDarkGray font-bold text-3xl mb-4'>Description</h4>
+                <p className='text-left px-2 mb-1'>
+                    COCO mall is a marketplace for all your needs, we offer a possibility to
+                    enterprises can create yours shops in we platform, in this app you can post a
+                    lot of products, share the ubication of store with Google Maps API and
+                    management of stock. The user can explore the all stores with complete
+                    flexibility, can add to cart a products of different stores, and realize the
+                    order in one payment.
+                </p>
+                <p className='text-left px-2'>
+                    Users can post reviews of the stores, and public questions in the products. The
+                    page count with panel of user and stores, in this place you can administrate a
+                    store, products, orders, user info, etc.. Also we have a user administrator he
+                    can control all the stores, products and users.
+                </p>
+                <h4 className='text-myDarkGray font-bold text-3xl mb-4 mt-4'>Experience</h4>
+                <p className='text-left px-2'>
+                    During the project, I learned different things, mainly teamwork. This work was
+                    done by 8 people, where we had 1 month to carry it out. Part of the learning was
+                    using the SCRUM methodology, version management with GitHub and the discussions
+                    of how we were going to organize the tasks. It is a project where I learned a
+                    lot and had fun doing it.
+                </p>
+            </div>
         </div>
+    </div>
+</div>
     );
 }
 
 export default CocoMall;
+
